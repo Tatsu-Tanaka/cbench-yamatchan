4d2ae17f175ed1af612279fad8ef7a199e88621e
yamatchan <u.ma.ymd@gmail.com>
2016-10-06 05:43:21 +0900

    レポート課題(16.10.12)完了

--- lib/cbench.rb
+++ lib/cbench.rb
@@ -7,7 +7,7 @@ class Cbench < Trema::Controller
   def packet_in(datapath_id, message)
     send_flow_mod_add(
       datapath_id,
-      match: ExactMatch.new(message),
+      match: Match.new(in_port: message.in_port),
       buffer_id: message.buffer_id,
       actions: SendOutPort.new(message.in_port + 1)
     )
